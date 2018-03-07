c0aa82a6e89255974cc7cb38b1c9b97dacdc9bc1
Loka Kanarp <lokakanarp@gmail.com>
2018-03-07 20:15:30 +0100

    making function to get books from Libris API

--- scripts/main.js
+++ scripts/main.js
@@ -2,20 +2,30 @@ const nobelPrizeLaureates = document.getElementById("nobelPrizeLaureates");
 const addButton = document.getElementById("addButton");
 const input = document.getElementById("input");
 const errorMessage = document.getElementById("errorMessage");
-var date = new Date();
-var year = date.getFullYear();
 
-//A function to display Laureates at the webpage
+
+//A function to display Laureates at the webpage and handing the name to getLibrisBooks function
 function displayLaureates(laureatesArray) {
-	let lauretesHtml = "";
-	lauretesHtml =
-		`<p>${laureatesArray.prizes[0].laureates[0].firstname}</p>
-			<p>${laureatesArray.prizes[0].laureates[0].surname}</p>`;
-	nobelPrizeLaureates.innerHTML = lauretesHtml;
+	let laureatesHtml = "";
+	if(laureatesArray.prizes.length > 0){
+	let laureateName = `${laureatesArray.prizes[0].laureates[0].firstname} ${laureatesArray.prizes[0].laureates[0].surname}`;
+		console.log(laureateName);
+	getLibrisBooks(laureateName);
+	for(laureate of laureatesArray.prizes[0].laureates){
+		laureatesHtml +=
+		`<p>${laureate.firstname}
+			${laureate.surname}</p>`;
+	}
+	}else {
+		nobelPrizeLaureates.innerHTML = "";
+		errorMessage.innerHTML = `<p>No one was awarded this year. Please try again.</p>`;
+	}
+	nobelPrizeLaureates.innerHTML = laureatesHtml;
 } //End of displayLauretes function
 
-//A function to get Laureates from API
+//A function to get Laureates from Nobel prize API
 function getLaureates(searchValue) {
+	//var idOfLaureate = 0;
 	fetch(`http://api.nobelprize.org/v1/prize.json?year=${searchValue}&category=literature`)
 		.then(function (response) {
 			return response.json();
@@ -28,9 +38,28 @@ function getLaureates(searchValue) {
 		})
 } //End of getLaureates function
 
-//A function to get libris from API
-function getLibrisBooks() {
-	fetch(`http://libris.kb.se/xsearch?query=W.V.+Quine&format=json`)
+//A function that checks the input value before the fetch is made.
+function checkInputNumber(searchValue) {
+	var date = new Date();
+	var year = date.getFullYear();
+	if(searchValue >= 1901 && searchValue <= year - 1){
+		getLaureates(searchValue);
+		errorMessage.innerHTML = "";
+	} else {
+		nobelPrizeLaureates.innerHTML = "";
+		errorMessage.innerHTML = `<p>We could not find a laureate for this year. Please try again.</p>`;
+	}
+} //End of checkInputNumber function
+
+
+//Functions concerning Libris API
+
+
+
+
+//A function to get books from Libris API
+function getLibrisBooks(laureateName) {
+	fetch(`http://libris.kb.se/xsearch?query=${laureateName}&format=json`)
 		.then(function (response) {
 			return response.json();
 		})
@@ -46,15 +75,6 @@ getLibrisBooks();
 
 
 
-function checkInputNumber(searchValue) {
-	if(searchValue >= 1901 && searchValue <= year - 1){
-		getLaureates(searchValue);
-		errorMessage.innerHTML = "";
-	} else {
-		nobelPrizeLaureates.innerHTML = "";
-		errorMessage.innerHTML = `<p>We could not find a laureate for this year. Please try again.</p>`;
-	}
-}
 
 addButton.addEventListener('click', function () {
 	const searchValue = input.value;
