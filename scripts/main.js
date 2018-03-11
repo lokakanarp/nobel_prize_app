762c7833871f63a0ba7aa85c21f8125acb35c733
Loka Kanarp <lokakanarp@gmail.com>
2018-03-11 14:48:22 +0100

    Displaying year and fixed problem with booktitles

--- scripts/main.js
+++ scripts/main.js
@@ -1,34 +1,41 @@
+const input = document.getElementById("input");
+const searchButton = document.getElementById("searchButton");
+const errorMessage = document.getElementById("errorMessage");
+const year = document.getElementById("yearp");
 const nobelPrizeLaureates = document.getElementById("nobelPrizeLaureates");
 const bookTitles = document.getElementById("bookTitles");
-const addButton = document.getElementById("addButton");
-const input = document.getElementById("input");
-const errorMessage = document.getElementById("errorMessage");
+const motivation = document.getElementById("motivation");
 
 
-//A function to display Laureates at the webpage and handing the name to getLibrisBooks function
+
+/*A function to display laureates at the webpage and 
+handing the laureate name to getLibrisBooks function*/
 function displayLaureates(laureatesData) {
 	let laureatesHtml = "";
+	let motivationHtml = "";
 	if (laureatesData.prizes.length > 0) {
-		
 		for (laureate of laureatesData.prizes[0].laureates) {
 			let laureateSurName = `${laureate.surname}`;
-		let laureateFirstName = `${laureate.firstname}`;
-		console.log(laureateFirstName, laureateSurName);
-		getLibrisBooks(laureateFirstName, laureateSurName);
+			let laureateFirstName = `${laureate.firstname}`;
+			console.log(laureateFirstName, laureateSurName);
+			getLibrisBooks(laureateFirstName, laureateSurName);
 			laureatesHtml +=
 				`<p>${laureate.firstname}
 				${laureate.surname}</p>`;
+			motivationHtml += `<p>${laureate.motivation}</p>`;
 		}
 	} else {
 		nobelPrizeLaureates.innerHTML = "";
+		motivation.innerHTML = "";
+		bookTitles.innerHTML = "";
 		errorMessage.innerHTML = `<p>No one was awarded this year. Please try again.</p>`;
 	}
 	nobelPrizeLaureates.innerHTML = laureatesHtml;
+	motivation.innerHTML = motivationHtml;
 } //End of displayLauretes function
 
 //A function to get Laureates from Nobel prize API
 function getLaureates(searchValue) {
-	//var idOfLaureate = 0;
 	fetch(`http://api.nobelprize.org/v1/prize.json?year=${searchValue}&category=literature`)
 		.then(function (response) {
 			return response.json();
@@ -54,22 +61,40 @@ function checkInputNumber(searchValue) {
 	}
 } //End of checkInputNumber function
 
-
 //Functions concerning Libris API
 
 //A function to display booktitles at the webpage.
 function displayLibrisBooks(librisBooksData) {
 	let booksHtml = "";
 	let booksArray = [];
+	let filteredArray = [];
 	for (book of librisBooksData.xsearch.list) {
 		if (book.language == "eng" && book.type == "book") {
-			booksArray.push(book.title);
+			booksArray.push(book.title.toUpperCase());
+		}
+		booksArray.forEach(function (item) {
+		if (filteredArray.indexOf(item) < 0) {
+			filteredArray.push(item);
+		}
+	});
+		//console.log(filteredArray)
+	}
+	//console.log(booksArray);
+	
+	
+	if (filteredArray.length > 8){
+		for (let i = 0; i < 8; i++) {
+			booksHtml += `<p>${filteredArray[i]}</p>`;
+		}
+	} else {
+		for (item of filteredArray){
+			booksHtml += `<p>${item}</p>`;
 		}
 	}
-	console.log(booksArray);
-	//nobelPrizeLaureates.innerHTML = booksHtml;
-} //End of displayLauretes function
-
+	console.log(booksHtml);
+	//bookTitles.innerHTML = booksHtml;
+	bookTitles.insertAdjacentHTML('beforeend', booksHtml);
+} //End of displayLibrisBooks function
 
 //A function to get books from Libris API
 function getLibrisBooks(laureateFirstName, laureateSurName) {
@@ -101,11 +126,15 @@ function getLibrisBooks(laureateFirstName, laureateSurName) {
 		})
 } //End of getLibrisBooks function
 
+//Function to display year
+function displayYear(searchValue) {
+	var dividedYear = `${searchValue.substring(0, 2)}<br>${searchValue.substring(2, 4)}`;
+	yearp.innerHTML = dividedYear;
+}
 
-
-
-addButton.addEventListener('click', function () {
+searchButton.addEventListener('click', function () {
 	const searchValue = input.value;
+	displayYear(searchValue);
 	checkInputNumber(searchValue);
 	input.value = "";
 })
